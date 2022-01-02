819f0c5e1286151de3ceb6bbe6904aa79fe6afb1
keroken <kenichi.shinagawa@gmail.com>
2022-01-02 18:17:20 +0900

    update playgrounds/.gitignore and import css from scss lib

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { Button } from '@ds-ex/react';
 
+import '@ds-ex/scss/lib/Button.css';
+
 ReactDOM.render(
   <Button label="Example Button" />,
   document.querySelector('#root')
