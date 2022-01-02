cdb4a0e1ed7ac84edd2f558cac2eca4aefd28277
keroken <kenichi.shinagawa@gmail.com>
2022-01-02 17:45:09 +0900

    create playgrounds package and react to display components

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Button } from '@ds-ex/react';
+
+ReactDOM.render(
+  <Button label="Example Button" />,
+  document.querySelector('#root')
+);
